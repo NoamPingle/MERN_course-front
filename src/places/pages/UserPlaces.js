05e50d89d9cfb4353aaecd32588e54d9ff46c8d4
PingleNoam <pinglenoam@gmail.com>
2022-10-16 15:38:03 +0300

    what

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -1,40 +1,40 @@
-import React from "react";
-import { useParams } from "react-router-dom";
-import PlaceList from "../components/PlaceList";
+import React from 'react';
+import { useParams } from 'react-router-dom';
+
+import PlaceList from '../components/PlaceList';
 
 const DUMMY_PLACES = [
   {
-    id: "p1",
-    title: "Binian Gadol",
-    description: "A very big building!",
-    imgageUrl:
-      "https://upload.wikimedia.org/wikipedia/commons/b/b3/Wikipedia-logo-v2-en.svg",
-    address: "Jump st. 21 new ashton",
+    id: 'p1',
+    title: 'Empire State Building',
+    description: 'One of the most famous sky scrapers in the world!',
+    imageUrl:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
+    address: '20 W 34th St, New York, NY 10001',
     location: {
-      lat: 32.834722,
-      lng: -83.651667,
+      lat: 40.7484405,
+      lng: -73.9878584
     },
-    creator: "u1",
+    creator: 'u1'
   },
-
   {
-    id: "p2",
-    title: "Binian Gadol",
-    description: "A very big building!",
-    imgageUrl:
-      "https://upload.wikimedia.org/wikipedia/commons/b/b3/Wikipedia-logo-v2-en.svg",
-    address: "Jump st. 21 new ashton",
+    id: 'p2',
+    title: 'Empire State Building',
+    description: 'One of the most famous sky scrapers in the world!',
+    imageUrl:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
+    address: '20 W 34th St, New York, NY 10001',
     location: {
-      lat: 32.834722,
-      lng: -83.651667,
+      lat: 40.7484405,
+      lng: -73.9878584
     },
-    creator: "u2",
-  },
+    creator: 'u2'
+  }
 ];
 
 const UserPlaces = () => {
   const userId = useParams().userId;
-  const loadedPlaces = DUMMY_PLACES.filter((place) => place.creator === userId);
+  const loadedPlaces = DUMMY_PLACES.filter(place => place.creator === userId);
   return <PlaceList items={loadedPlaces} />;
 };
 
