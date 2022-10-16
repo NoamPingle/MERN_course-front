42345d119504564edc36933b9a5569635ce11628
PingleNoam <pinglenoam@gmail.com>
2022-10-16 13:03:14 +0300

    added map and Form

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,16 +1,38 @@
-import React from "react";
+import React, { useCallback, useReducer } from "react";
+
 import Input from "../../shared/components/FormElements/Input";
+import {
+  VALIDATOR_MINLENGTH,
+  VALIDATOR_REQUIRE,
+} from "../../shared/util/validators";
+
 import "./NewPlace.css";
 
+const formReducer = {[]}
+
 const NewPlace = () => {
+  useReducer();
+  const titleInputHadnler = useCallback((id, value, isValid) => {}, []);
+  const descriptionInputHadnler = useCallback((id, value, isValid) => {}, []);
+
   return (
     <form className="place-form">
       <Input
+        id="title"
         element="input"
         type="text"
         label="Title"
-        validators={[]}
+        validators={[VALIDATOR_REQUIRE()]}
         errorText="Please Enter a valid title."
+        onInput={titleInputHadnler}
+      />
+      <Input
+        id="description"
+        element="texterea"
+        label="Description"
+        validators={[VALIDATOR_MINLENGTH(5)]}
+        errorText="Please Enter a valid description. (MIN 5 characters)"
+        onInput={descriptionInputHadnler}
       />
     </form>
   );
